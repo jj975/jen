f4656c62c9940b541a13ac7085619337913b26c8
jj975 <126405323+jj975@users.noreply.github.com>
2023-07-19 19:33:10 +0300

    Add files via upload

---
+++ sourse/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  type = string
+  default = "AKIAQGL7X2SFCD722SG4"
+}
+
+variable "aws_secret_key" {
+  type = string
+  default = "QreURtOvkcVmhbNSuRgnyrYtrDzFUnv7CYhMZsdf"
+}
