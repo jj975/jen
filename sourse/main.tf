4e3bc0f56fa1d921d3383166ed241bba5a1344e3
jj975 <126405323+jj975@users.noreply.github.com>
2023-07-19 20:06:18 +0300

    Add files via upload

--- sourse/main.tf
+++ sourse/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "eu-north-1"
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    access_key = "AKIAQGL7X2SFCD722SG4"
+    secret_key = "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"
 }
 
 variable "users" {
@@ -66,4 +66,69 @@ output "account_ids" {
         for user, user_data in data.aws_iam_user.current :
         user => user_data.id
     }
+}
+
+
+resource "aws_instance" "EC2-Instance" {
+  availability_zone = "eu-north-1a"
+  count             = 2
+  ami               = "ami-0989fb15ce71ba39e"
+  instance_type     = "t3.micro"
+  key_name          = "key"
+  vpc_security_group_ids = [aws_security_group.tersec[count.index].id]
+
+  ebs_block_device {
+    device_name           = "/dev/sda1"
+    volume_size           = 10
+    volume_type           = "standard"
+    delete_on_termination = true
+    tags                  = {
+      Name = "root-disk"
+    }
+  }
+
+  tags = {
+    Name = "EC2-Instance"
+  }
+}
+
+resource "aws_security_group" "tersec" {
+  count       = 2
+  name        = "tersec-${count.index}"
+  description = "uff - 165 social credit"
+
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    description = "Allow 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow ping"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "elastic_ip" {
+  count = 2
+  instance = aws_instance.EC2-Instance[count.index].id
+}
+
+output "elastic_ips" {
+  value = aws_eip.elastic_ip.*.public_ip
 }
